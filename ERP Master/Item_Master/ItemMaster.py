241fd0b94fec71b66acc6f352e5ca8fef27ed8d0
Omkar <omkarkadam058@gmail.com>
2025-03-03 12:52:51 +0530

    Commit

--- ERP Master/Item_Master/ItemMaster.py
+++ ERP Master/Item_Master/ItemMaster.py
@@ -42,12 +42,14 @@ class ItemMasterTest(unittest.TestCase):
         driver = self.driver
         driver.switch_to.default_content()
 
-        for index, iframe in enumerate(driver.find_elements(By.TAG_NAME, "iframe")):
+        for iframe in self.driver.find_elements(By.TAG_NAME, "iframe"):
             driver.switch_to.frame(iframe)
             if driver.find_elements(By.ID, element_id):
                 return True
+            print(f"Switched to iframe containing {element_id}")
 
             driver.switch_to.default_content()
+        print(f"Unable to locate {element_id} in any iframe!")
         return False
 
     def test_item_master(self):
@@ -78,7 +80,7 @@ class ItemMasterTest(unittest.TestCase):
             self.click_element(By.ID, "btn_NewRecord")
 
         if self.switch_to_iframe("TransportProductName"):
-            self.send_keys(By.ID, "TransportProductName", "TEST7")
+            self.send_keys(By.ID, "TransportProductName", "TEST8")
 
         if self.switch_to_iframe("CommodityTypeId"):
             self.select_dropdown(By.ID, "CommodityTypeId", "TEST1")
