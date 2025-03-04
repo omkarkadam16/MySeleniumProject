15ff14b4dda8c96602f33c99445aad18f98052aa
Omkar <omkarkadam058@gmail.com>
2025-03-04 16:08:50 +0530

    Commit

--- ERP Master/NavigationLinks.py
+++ ERP Master/NavigationLinks.py
@@ -6,38 +6,50 @@ from selenium.webdriver.support import expected_conditions as EC
 import unittest
 
 
-class LoginTest(unittest.TestCase):
+class LoginPage(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # Provide the path to your ChromeDriver
         cls.driver = webdriver.Chrome(
             service=Service(r"C:\Users\user\Downloads\WebDrivers\chromedriver.exe")
         )
         cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
 
-    def test_commodity(self):
-        driver = self.driver
-        driver.get("http://192.168.0.72/Rlogic9RLS/Login")
-
-        driver.find_element(By.ID, "Login")
-        driver.find_element(By.ID, "Login").send_keys("Riddhi")
-        driver.find_element(By.ID, "Password").send_keys("OMSGN9")
-        driver.find_element(By.ID, "btnLogin").click()
-
-        WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.LINK_TEXT, "Transportation"))
+    def click_element(self, by, value, timeout=5):
+        WebDriverWait(self.driver, timeout).until(
+            EC.element_to_be_clickable((by, value))
         ).click()
-        print("Transportation link clicked successfully")
 
-        menu_items = [
-            ("Transportation Master »", "Transportation Master"),
-            ("Common Masters »", "Common Masters"),
-            ("Item Master", "Item Master"),
+    def send_keys(self, by, value, text, timeout=5):
+        WebDriverWait(self.driver, timeout).until(
+            EC.visibility_of_element_located((by, value))
+        ).send_keys(text)
+
+    def test_Commodity_Master(self):
+        driver = self.driver
+        driver.get("http://r-logic9.com/RlogicDemoFtl/Login")
+
+        self.send_keys(By.ID, "Login", "Riddhi")
+        self.send_keys(By.ID, "Password", "OMSGN9")
+        self.click_element(By.ID, "btnLogin")
+        print("Login successful")
+
+        menus = [
+            "Transportation",
+            "Transportation Master »",
+            "Common Masters »",
+            "Item Master",
         ]
 
-        for link_text, description in menu_items:
-            WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((By.LINK_TEXT, link_text))
-            ).click()
-            print(f"{description} link clicked successfully")
+        for links in menus:
+            self.click_element(By.LINK_TEXT, links)
+            print(f"{links} link clicked successfully")
+
+    @classmethod
+    def tearDownClass(cls):
+        """Closes the browser after tests are complete."""
+        cls.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
