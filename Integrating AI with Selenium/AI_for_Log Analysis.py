9c599f91bdf46d04f6d8af10161c235ca27a8ca9
Omkar <omkarkadam058@gmail.com>
2025-03-13 16:27:24 +0530

    Commit

--- Integrating AI with Selenium/AI_for_Log Analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sklearn.cluster import KMeans
-
-df = pd.read_csv("test_logs.csv")
-model = KMeans(n_clusters=3)
-df["Cluster"] = model.fit_predict(df[["execution_time", "error_count"]])
-
-print(df.head())
