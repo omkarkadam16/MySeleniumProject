7e8d55fa20a599edd2839f2f8bdd2ae140ca6144
Omkar <omkarkadam058@gmail.com>
2025-03-31 13:22:07 +0530

    Commit

--- System Testing/MYSQL/1_MySQL Masters/21_Document_Mapping/4_DM_CashInHand.py
+++ System Testing/MYSQL/1_MySQL Masters/21_Document_Mapping/4_DM_CashInHand.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
-class DocumentMapping(unittest.TestCase):
+class DMCashInHand(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -78,7 +78,7 @@ class DocumentMapping(unittest.TestCase):
         input_text.send_keys(Keys.DOWN)
         input_text.send_keys(Keys.ENTER)
 
-    def test_route(self):
+    def test_DM_CashInHand(self):
         driver = self.driver
         driver.get("http://192.168.0.72/Rlogic9UataScript?ccode=UATASCRIPT")
 
